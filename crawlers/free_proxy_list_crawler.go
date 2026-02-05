44511412925c0ceef539861dff6da0e6f368a5fb
Md Masud karim <msmasud578@gmail.com>
2026-02-05 20:24:48 +0100

    Added Crawler interface and it's implementations

--- crawlers/free_proxy_list_crawler.go
+++ crawlers/free_proxy_list_crawler.go
@@ -3,7 +3,9 @@ package crawlers
 import "fmt"
 import "github.com/Masud2017/proxy_crawler/models"
 
-func Crawl() {
+type FreeProxyListCrawler struct {}
+
+func (freeProxyListCrawler FreeProxyListCrawler) Crawl() {
 	var data models.CrawledData = models.CrawledData{}
 	
 	fmt.Println("Hello world this is my test data: ",data)
