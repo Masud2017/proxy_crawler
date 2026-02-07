a692029e7b3e8aebe617bf335cb07243929376fe
Md Masud karim <msmasud578@gmail.com>
2026-02-07 16:22:41 +0100

    Updated the configuration code

--- configs/config.go
+++ configs/config.go
@@ -1,2 +1,27 @@
 package configs
 
+import "fmt"
+import "os"
+
+type Configuration struct {
+	IsProd bool
+}
+
+IsProd := false
+if os.Getenv("dev_type") == "prod" {
+	IsProd = true
+}
+
+
+func (conf Configuration) InitiateConfiguration () (Configuration, error){
+	// this will be the entry point for all the initial configuration that is needed for runtime execution
+	
+	conf := Configuration{ IsProd : IsProd}
+
+	if conf != nil {
+		return conf, Errors.new("The configuration is created successfully")
+	} else{
+		return conf, Errors.new("Something went wrong the configuration is unsuccessful.")
+	}
+
+}
