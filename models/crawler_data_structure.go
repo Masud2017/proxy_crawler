6ca44bde32ab7049188868535f3e1ece6d6afa2e
Md Masud karim <msmasud578@gmail.com>
2026-02-10 23:49:01 +0100

    updated the crawler_data_structure.go file

--- models/crawler_data_structure.go
+++ models/crawler_data_structure.go
@@ -6,4 +6,11 @@ type CrawledData struct {
 	DataMap map[string]map[string]int // this is the mapped version can be useful for many tasks also faster to access.
 }
 
+func (crawledData CrawledData) getDataSize() int {
+	return crawledData.DataSize
+}
+
+func (crawledData CrawledData) getData() string {
+	return crawledData.Data
+}
 
