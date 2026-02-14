eb55737da6c0a4ebd267f71d51b30cff85726023
Md Masud karim <msmasud578@gmail.com>
2026-02-14 21:13:42 +0100

    updated the carawler_data_structure, free_proxy_list_crawler and config

--- crawlers/free_proxy_list_crawler.go
+++ crawlers/free_proxy_list_crawler.go
@@ -2,11 +2,18 @@ package crawlers
 
 import "fmt"
 import "github.com/Masud2017/proxy_crawler/models"
+import "github.com/anaskhan96/soup"
 
 type FreeProxyListCrawler struct {}
 
 func (freeProxyListCrawler FreeProxyListCrawler) Crawl() {
 	var data models.CrawledData = models.CrawledData{}
+	var url := ""
+	resp, err := soup.Get("https://free-proxy-list.net/en/")
+        if (err != nil) {
+		fmt.Println("There was a problem during parsing the html content. Please check the free-proxy-list crawler.")
+                os.Exit(1)
+        }
 	
 	fmt.Println("Hello world this is my test data: ",data)
 }
--- models/crawler_data_structure.go
+++ models/crawler_data_structure.go
@@ -1,9 +1,10 @@
 package models
 
 type CrawledData struct {
-	DataSize int
+	DataSize int // the data size measuring unit should be bytes
 	Data string // this is the texual version of the data. In future i might need to replace it with a file descriptor since the scrapped list can be in any size. Putting scrapped data all in memory at once can result in memory shortage.
 	DataMap map[string]map[string]int // this is the mapped version can be useful for many tasks also faster to access.
+	DataMapInterface interface{} // this is the optional type Data mapper that can be casted to different data types.
 }
 
 func (crawledData CrawledData) getDataSize() int {
